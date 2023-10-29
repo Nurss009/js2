3fc95cd11b25cc2137a4c8b07527c446d3adc99e
Nursultan <tupoukidala77@gmail.com>
2023-10-29 19:57:25 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,131 @@
+// function myFunction(user) {
+//     if (user.toLowerCase() === 'admin') {
+//         return 'Welcom admin '
+//     }
+//     else{
+//         return 'false not correct user '
+//     }
+// }
+////
+// console.log(myFunction('admin'))
+// function myFunction(days) {
+//     if (days >=0 && days <=28) {
+//         return ` today is ${days} of february`    }
+//     else{
+//         return 'on february only 28 day, your day is day of another mon'
+//     }
+// }
+// console.log(myFunction(29))
+////
+// function myFunction(days) {
+//     if (days == 50) {
+//         return 'num is equals of 50'    }
+//     else{
+//         return 'num is not correct'
+//     }
+// }
+// console.log(myFunction(50))
+////
+// function myFunction( user,age,old) {
+//     if (age >=18 && age <=50) {
+//         return ` ${user} ${age} ${old} welcom`    }
+//     else{
+//         return ` ${user} ${age} ${old} rejected`
+//     }
+// }
+// console.log(myFunction( 'nurss',17, 20))
+////
+// function myFunction(user) {
+//     if (user.length<3)  {
+//         return `${user} name is too short (min 3)`    }
+//     else{
+//         return `${user} name is accepted`
+//     }
+// }
+// console.log(myFunction('aass'))
+////
+// function myFunction(user) {
+//     if (user.length>5)  {
+//         return `${user} name is too long (max 5)`    }
+//     else{
+//         return `${user} name is accepted`
+//     }
+// }
+// console.log(myFunction('aaf'))
+////
+// function ageFunction(age) {
+//     if (typeof age === "number") {
+//         return `${age} old`
+//     } else{
+//         return `${age} must be a number`
+//     }
+// }
+// console.log(ageFunction(20))
+////
+// function ageFunction(age) {
+//     if (age >23)
+//     {
+//         return `${age} old`
+//     } else{
+//         return `${age} must be older then 23`
+//     }
+// }
+// console.log(ageFunction(22))
+////
+// function myFunction(num) {
+//     if (num === 1) {
+//         return ' today is monday'
+//     }
+//     else if (num === 2) {
+//         return ' today is monday'
+//     }
+//     else if (num === 3) {
+//         return ' today is monday'
+//     }
+//     else if (num === 4) {
+//         return ' today is monday'
+//     }
+//     else if (num === 5) {
+//         return ' today is monday'
+//     }
+//     else if (num === 6) {
+//         return ' today is monday'
+//     }
+//     else if (num === 7) {
+//         return ' today is monday'
+//     }
+//     else  {
+//         return 'max days of weeks 7'
+//     }
+// }
+// console.log(myFunction(8))
+////
+// function myFunction(a){
+//     if(a.length%2===0){
+//         return `длина ${a} четная`
+//     }else{
+//         return `длина ${a} нечетная`
+//     }
+// }
+// console.log(myFunction('Function'))
+// console.log(myFunction('Funct'))
+////
+// function myFunction(str,a){
+//     if(str.includes(" ")){
+//         a=str.replaceAll(' ','')
+//         return a
+//     }
+// }
+// console.log(myFunction('lorem ipsum dolor sit amet'))
+////
+// function myFunction(str){
+//   if(typeof str ==='string' && str.length>=4 && str.length<=10 && str.length%2===0){
+//       return 'Это строка не пустая, где символ больше 4 и меньше 10, также она четная'
+//   }
+//   else if (str.length===0 || str.length%2 || str.length<4 || str.length>10){
+//       return 'это пустая строка или нечетная, где длина меньше 4 и больше 10'
+//   }
+// }
+// console.log(myFunction('function'))
+// console.log(myFunction('func'))
+// console.log(myFunction(''))
